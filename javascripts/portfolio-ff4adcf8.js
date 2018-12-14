5cc0ab6c89a59f3ffdef8424fc874962333630ce
Sz-Ting, Chen <kimikamis@gmail.com>
2018-12-14 15:23:20 +0800

    Automated commit at 2018-12-14 07:23:20 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/portfolio-ff4adcf8.js
@@ -0,0 +1,1 @@
+$(function(){var t="";$(".fil-cat").click(function(){t=$(this).attr("data-rel"),$("#portfolios-list").fadeTo(100,.1),$("#portfolios-list div").not("."+t).fadeOut().removeClass("scale-anm"),setTimeout(function(){$("."+t).fadeIn().addClass("scale-anm"),$("#portfolios-list").fadeTo(300,1)},300)})});
