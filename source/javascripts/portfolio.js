c2ae2d9d2f09b21b6e226ebbef25aa9b9167652f
Sz-Ting, Chen <kimikamis@gmail.com>
2018-12-16 15:17:08 +0800

    change skills cards/fix banner img/add animation to portfolios cards

--- source/javascripts/portfolio.js
+++ source/javascripts/portfolio.js
@@ -1,13 +1,12 @@
 $(function() {
-    var selectedClass = "";
-    $(".fil-cat").click(function(){
+  var selectedClass = "";
+  $(".fil-cat").click(function(){
     selectedClass = $(this).attr("data-rel");
-     $("#portfolios-list").fadeTo(100, 0.1);
-    $("#portfolios-list div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
+    $("#portfolios-list").fadeTo(100, 0.1);
+    $("#portfolios-list > div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
     setTimeout(function() {
       $("."+selectedClass).fadeIn().addClass('scale-anm');
       $("#portfolios-list").fadeTo(300, 1);
     }, 300);
-
   });
 });
